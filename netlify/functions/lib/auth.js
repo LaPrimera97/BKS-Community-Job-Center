d76b12a3a1c411e717fcc8e789e0c7ab64bc8f7d
LaPrimera97 <briankagisokingsley@gmail.com>
2026-07-24 07:51:02 +0200

    Update auth.js

--- netlify/functions/lib/auth.js
+++ netlify/functions/lib/auth.js
@@ -1,1 +1,16 @@
+const crypto = require('crypto');
 
+function hashPassword(password) {
+  if (!password) return '';
+  return crypto.createHash('sha256').update(password, 'utf8').digest('hex');
+}
+
+function checkAdminPassword(password) {
+  const correct = process.env.ADMIN_PASSWORD;
+  if (!correct) {
+    throw new Error('ADMIN_PASSWORD environment variable is not set.');
+  }
+  return password === correct;
+}
+
+module.exports = { hashPassword, checkAdminPassword };
